20884d479a4c861e47ee119615733ea404172f34
denisktoo <146120658+denisktoo@users.noreply.github.com>
2025-08-28 21:01:09 +0300

    Schedule a GraphQL Mutation for Product Stock Alerts

--- crm/schema.py
+++ crm/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Customer, Product, Order
+from crm.models import Customer, Product, Order
 import re
 from django.core.exceptions import ValidationError
 from datetime import datetime
